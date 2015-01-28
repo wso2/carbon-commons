e1013fc127b0a24b81a36c893914407fe0a186f7
manoj-kumara <makuma87@gmail.com>
2015-01-28 14:46:47 +0530

    removing registry based DepSync components from carbon-commons

---
+++ components/deployment-synchronizer/org.wso2.carbon.deployment.synchronizer/src/main/java/org/wso2/carbon/deployment/synchronizer/util/RepositoryConfigParameter.java
@@ -0,0 +1,100 @@
+/*
+*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.wso2.carbon.deployment.synchronizer.util;
+
+public class RepositoryConfigParameter {
+
+    /**
+     * The name of Parameter.
+     */
+    private String name;
+
+    /**
+     * The value the parameter will be holding
+     */
+    private String value;
+
+    /**
+     * Indicates whether this is a mandatory field or not
+     */
+    private boolean required;
+
+    /**
+     * Whether the field is masked (ex: for password fields)
+     */
+    private boolean masked;
+
+    /**
+     * The maximum number of characters allowed
+     */
+    private int maxlength = 25;
+
+    /**
+     * Type of the parameter (ex: String, Boolean)
+     */
+    private String type;
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMasked() {
+        return masked;
+    }
+
+    public void setMasked(boolean masked) {
+        this.masked = masked;
+    }
+
+    public int getMaxlength() {
+        return maxlength;
+    }
+
+    public void setMaxlength(int maxlength) {
+        this.maxlength = maxlength;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
