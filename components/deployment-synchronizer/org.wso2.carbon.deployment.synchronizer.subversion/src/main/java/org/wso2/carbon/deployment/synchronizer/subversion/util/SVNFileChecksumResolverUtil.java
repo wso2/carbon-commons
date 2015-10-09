b318e5ec24320b5b485369aabcb703d3eb8739ee
Thusitha Dayaratne <thusithad@wso2.com>
2015-10-09 10:09:27 +0530

    COMMONS-126: Change implementation to use try-with-resources

--- components/deployment-synchronizer/org.wso2.carbon.deployment.synchronizer.subversion/src/main/java/org/wso2/carbon/deployment/synchronizer/subversion/util/SVNFileChecksumResolverUtil.java
+++ components/deployment-synchronizer/org.wso2.carbon.deployment.synchronizer.subversion/src/main/java/org/wso2/carbon/deployment/synchronizer/subversion/util/SVNFileChecksumResolverUtil.java
@@ -18,6 +18,11 @@
 
 package org.wso2.carbon.deployment.synchronizer.subversion.util;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -27,11 +32,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.context.PrivilegedCarbonContext;
-import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 /**
  * This class resolves the checksum issues causing svn 1.6 in the .svn/entries file.
@@ -89,15 +89,10 @@ public class SVNFileChecksumResolverUtil {
             log.debug("Trying to correct the checksum mismatch for SVN file:" +fullSvnFilePath+ "." +
             		"Expected:" +expectedChecksum.trim()+ " but it is:" +actualChecksum.trim());
 
-            Reader fis = null;
-            BufferedReader bis = null;
-            BufferedWriter bw = null;
-            try{
-                fis = new FileReader(errorFile);
-                
-                bis = new BufferedReader(new InputStreamReader(new FileInputStream(errorFile)));
-                bw = new BufferedWriter(new FileWriter(tmpFile));
-                
+            try (Reader fis = new FileReader(errorFile);
+                 BufferedReader bis = new BufferedReader(new InputStreamReader(new FileInputStream(errorFile)));
+                 BufferedWriter bw = new BufferedWriter(new FileWriter(tmpFile));) {
+
                 String line;
                 while((line = bis.readLine()) != null){   
                 	String plainLine = line.trim();
@@ -112,28 +107,6 @@ public class SVNFileChecksumResolverUtil {
                 }
             } catch(Exception e){
                 log.error(e.getMessage());
-            } finally {
-                try {
-                    if (bis != null) {
-                        bis.close();
-                    }
-                } catch (IOException e) {
-                    log.error("Couldn't close the BufferedReader " + e.getMessage(), e);
-                }
-                try {
-                    if (fis != null) {
-                        fis.close();
-                    }
-                } catch (IOException e) {
-                    log.error("Couldn't close the FileReader " + e.getMessage(), e);
-                }
-                try {
-                    if (bw != null) {
-                        bw.close();
-                    }
-                } catch (IOException e) {
-                    log.error("Couldn't close the BufferedWriter " + e.getMessage(), e);
-                }
             }
 
 //            Rename the tmp file and remove old file.
