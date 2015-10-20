75dea7c206faf3b4c48d8fc76392d683df9ef1ae
chanikag <chanika@wso2.com>
2015-10-20 14:36:10 +0530

    Fixing code formatting

--- components/ndatasource/org.wso2.carbon.ndatasource.ui/src/main/java/org/wso2/carbon/ndatasource/ui/NDataSourceClientConstants.java
+++ components/ndatasource/org.wso2.carbon.ndatasource.ui/src/main/java/org/wso2/carbon/ndatasource/ui/NDataSourceClientConstants.java
@@ -3,6 +3,6 @@ package org.wso2.carbon.ndatasource.ui;
 public class NDataSourceClientConstants {
 	
 	public static final String RDBMS_DTAASOURCE_TYPE = "RDBMS";
-       public static final String RDBMS_EXTERNAL_DATASOURCE_PROVIDER="External Datasource";
+       public static final String RDBMS_EXTERNAL_DATASOURCE_PROVIDER = "External Datasource";
 	
 }
