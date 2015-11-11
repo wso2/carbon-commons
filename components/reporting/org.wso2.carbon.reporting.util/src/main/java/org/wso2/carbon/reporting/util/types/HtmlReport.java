fceeb648ce783fba5e301880dee43ed1e84aa10e
gothami <gothamisa92@gmail.com>
2015-11-11 16:03:40 +0530

    updated year of the license header

--- components/reporting/org.wso2.carbon.reporting.util/src/main/java/org/wso2/carbon/reporting/util/types/HtmlReport.java
+++ components/reporting/org.wso2.carbon.reporting.util/src/main/java/org/wso2/carbon/reporting/util/types/HtmlReport.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2009-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -83,7 +83,7 @@ public class HtmlReport {
             exporterHTML.exportReport();
 
         } catch (JRException e) {
-            throw new JRException("Error occurred exporting HTML report", e);
+            throw new JRException("Error occurred exporting HTML report ", e);
         }
         return outputStream;
 
