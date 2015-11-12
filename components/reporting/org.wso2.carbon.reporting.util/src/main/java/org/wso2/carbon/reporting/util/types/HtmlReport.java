1322035d1ae33ce37e54eaf5fd80917acfc8ffe7
gothami <gothamisa92@gmail.com>
2015-11-12 11:45:56 +0530

    html report image embedding and jasper report update

--- components/reporting/org.wso2.carbon.reporting.util/src/main/java/org/wso2/carbon/reporting/util/types/HtmlReport.java
+++ components/reporting/org.wso2.carbon.reporting.util/src/main/java/org/wso2/carbon/reporting/util/types/HtmlReport.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2009-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,16 @@ package org.wso2.carbon.reporting.util.types;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.export.JRHtmlExporter;
-import net.sf.jasperreports.engine.export.JRHtmlExporterParameter;
+import net.sf.jasperreports.engine.export.HtmlResourceHandler;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleHtmlExporterOutput;
+import org.apache.commons.codec.binary.Base64;
 import org.wso2.carbon.reporting.api.ReportingException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Generate HTML report using given jasperPrint object
@@ -53,20 +58,26 @@ public class HtmlReport {
 	    // exclude the page footers
 	    jasperPrint.setProperty("net.sf.jasperreports.export.html.exclude.origin.band.2", "pageFooter");
  
-            JRHtmlExporter jrHtmlExporter = new JRHtmlExporter();
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.JASPER_PRINT, jasperPrint);
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.IS_USING_IMAGES_TO_ALIGN, true);
-            // To generate a HTML report we want configure ImageServlet in component.xml file of reporting UI bundle
-            // Then want to set the  IMAGES_URI parameter
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.IMAGES_URI, "../servlets/image?image=");
-	    // remove extra spaces between the report data
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.BETWEEN_PAGES_HTML, "");           
-	   
- 	    // remove empty spaces
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE);
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.TRUE);
-            jrHtmlExporter.setParameter(JRHtmlExporterParameter.OUTPUT_STREAM, outputStream);
-            jrHtmlExporter.exportReport();
+            final Map<String, String> images = new HashMap<>();
+            JRHtmlExporter exporterHTML = new JRHtmlExporter();
+            //setting up an input stream to JRHtmlExporter object
+            SimpleExporterInput exporterInput = new SimpleExporterInput(jasperPrint);
+            exporterHTML.setExporterInput(exporterInput);
+            //setting up an output stream to JRHtmlExporter object
+            SimpleHtmlExporterOutput simpleHtmlExporterOutput = new SimpleHtmlExporterOutput(outputStream);
+            //To generate a html report we need to embed images using base64 encoding.
+            //Otherwise it shows corrupted images.
+            simpleHtmlExporterOutput.setImageHandler(new HtmlResourceHandler() {
+                @Override public void handleResource(String id, byte[] data) {
+                    images.put(id, "data:image/gif;base64," + new String(Base64.encodeBase64(data)));
+                }
+                @Override public String getResourcePath(String id) {
+                    return images.get(id);
+                }
+            });
+
+            exporterHTML.setExporterOutput(simpleHtmlExporterOutput);
+
 
         } catch (JRException e) {
             throw new JRException("Error occurred exporting HTML report ", e);
