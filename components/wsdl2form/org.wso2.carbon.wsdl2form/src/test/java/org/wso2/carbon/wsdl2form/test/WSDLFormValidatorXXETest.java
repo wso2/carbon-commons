e87f6b63a5b1991ad83a78e6bc4975cb69682e57
Vidura Nanayakkara <vidura_nanayakkara@hotmail.com>
2016-11-30 14:04:42 +0530

    Fix WSDL tryit validation issues
    Identitifed issues when validating WSDL XML input stream fixed

---
+++ components/wsdl2form/org.wso2.carbon.wsdl2form/src/test/java/org/wso2/carbon/wsdl2form/test/WSDLFormValidatorXXETest.java
@@ -0,0 +1,52 @@
+/*
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+package org.wso2.carbon.wsdl2form.test;
+
+import org.apache.axis2.AxisFault;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.wso2.carbon.wsdl2form.Util;
+import org.wso2.carbon.wsdl2form.test.constants.XXEResourceConstants;
+import org.wso2.carbon.wsdl2form.test.models.XXEResource;
+
+import java.io.File;
+
+/**
+ * Class for validating Util against XXE attacks.
+ */
+public class WSDLFormValidatorXXETest {
+
+    /**
+     * Test for provided XXE injections.
+     */
+    @Test
+    public void testXMLXXEValidation() {
+        for (XXEResource xxeResource : XXEResourceConstants.XXE_RESOURCE_LIST) {
+            String url = xxeResource.getURLForFile().toString();
+
+            try {
+                File file = Util.writeWSDLToFileSystemHelpler(url);
+                Assert.assertNotNull(file);
+            } catch (AxisFault e) {
+                Assert.assertTrue(xxeResource.getExpectedException()
+                        .equals(e.getMessage()));
+            }
+        }
+    }
+}
