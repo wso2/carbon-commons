37b7ecc61aea39c51073636e83609b3d33a0d32f
denuwanthi <denuwanthi.hasanthika@gmail.com>
2020-04-02 11:55:42 +0530

    Add license header

--- components/uuid-generator/org.wso2.carbon.uuid.generator/src/main/java/org/wso2/carbon/uuid/generator/TimestampSynchronizer.java
+++ components/uuid-generator/org.wso2.carbon.uuid.generator/src/main/java/org/wso2/carbon/uuid/generator/TimestampSynchronizer.java
@@ -1,18 +1,20 @@
-/* JUG Java Uuid Generator
+/*
+ *  Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Copyright (c) 2002- Tatu Saloranta, tatu.saloranta@iki.fi
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * Licensed under the License specified in the file LICENSE which is
- * included with the source code.
- * You may not use this file except in compliance with the License.
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-
 package org.wso2.carbon.uuid.generator;
 
 import java.io.IOException;
@@ -22,13 +24,15 @@ import java.io.IOException;
  * ensure that timestamp values used for generating time/location-based UUIDs
  * are monotonically increasing, as well as that only one such generator
  * is ever used on a single system, even in presence of multiple JVMs.
- *<p>
+ * <p>
  * The default implementation used by JUG is
  * {@link com.fasterxml.uuid.ext.FileBasedTimestampSynchronizer}.
  */
-public abstract class TimestampSynchronizer
-{
-    protected TimestampSynchronizer() { }
+public abstract class TimestampSynchronizer {
+
+    protected TimestampSynchronizer() {
+
+    }
 
     /**
      * Initialization method is will be called on an instance by
@@ -37,7 +41,7 @@ public abstract class TimestampSynchronizer
      * configured, and should be able to determine the first legal timestamp
      * value (return value). Method should also initialize any locking that
      * it does (if any), such as locking files it needs.
-     *<p>
+     * <p>
      * Return value thus indicates the lowest initial time value that can
      * be used by the caller that can not have been used by previous
      * incarnations of the UUID generator (assuming instance was able to
@@ -49,12 +53,12 @@ public abstract class TimestampSynchronizer
      * value at this point.
      *
      * @return First (and last) legal timestamp to use; <code>0L</code> if it
-     *  can not
-     *   determine it and caller can use whatever value (current timestamp)
-     *   it has access to.
+     * can not
+     * determine it and caller can use whatever value (current timestamp)
+     * it has access to.
      */
     protected abstract long initialize()
-        throws IOException;
+            throws IOException;
 
     /**
      * Method {@link UUIDTimer} will call if this synchronizer object is
@@ -63,7 +67,7 @@ public abstract class TimestampSynchronizer
      * terminates.
      */
     protected abstract void deactivate()
-        throws IOException;
+            throws IOException;
 
     /**
      * Method called by {@link UUIDTimer} to indicate that it has generated
@@ -72,11 +76,10 @@ public abstract class TimestampSynchronizer
      * value (and possible additional ones).
      *
      * @param now Timestamp value caller wants to use, and that the
-     *   synchronizer is asked to protect.
-     *
+     *            synchronizer is asked to protect.
      * @return First timestamp value that can NOT be used by the caller;
-     *    has to be higher than the input timestamp value
+     * has to be higher than the input timestamp value
      */
     protected abstract long update(long now)
-        throws IOException;
+            throws IOException;
 }
